43d2a3626dd364a9a11c7284957690e314db474a
Karang <arthur.hennequin@free.fr>
2020-05-28 00:04:14 +0200

    Add block.shapes property

--- example.js
+++ example.js
@@ -15,3 +15,6 @@ const BlockNew = require('./')('1.13.2')
 const redstoneBlock = BlockNew.fromStateId(3381, 0)
 
 console.log(redstoneBlock)
+
+console.log(BlockNew.fromStateId(1649, 0).shapes) // stairs
+console.log(BlockNew.fromStateId(1651, 0).shapes) // stairs
