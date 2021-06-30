c05a1da4b8c09949bb46c535577b6558605dbd0e
u9g <aibotted@gmail.com>
2021-06-29 21:08:31 -0400

    add some tests

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,13 +1,41 @@
 /* eslint-env jest */
 
-const Block = require('../')('1.15.2')
-const mcData = require('minecraft-data')('1.15.2')
-
 // https://minecraft.gamepedia.com/Breaking#Blocks_by_hardness
 describe('Dig time', () => {
-  test('dirt by hand', () => {
-    const block = Block.fromStateId(mcData.blocksByName.dirt.defaultState, 0)
-    const time = block.digTime(null, false, false, false)
-    expect(time).toBe(750)
+  describe('1.15.2', () => {
+    const Block = require('../')('1.15.2')
+    const mcData = require('minecraft-data')('1.15.2')
+    test('dirt by hand', () => {
+      const block = Block.fromStateId(mcData.blocksByName.dirt.defaultState, 0)
+      const time = block.digTime(null, false, false, false)
+      expect(time).toBe(750)
+    })
+  })
+  describe('1.17', () => {
+    const Block = require('../')('1.17')
+    const mcData = require('minecraft-data')('1.17')
+    test('instant break stone', () => {
+      const block = Block.fromStateId(mcData.blocksByName.stone.defaultState, 0)
+      const time = block.digTime(
+        mcData.itemsByName.diamond_pickaxe.id,
+        false,
+        false,
+        false,
+        [{ id: mcData.enchantmentsByName.efficiency.name, lvl: 5 }],
+        {
+          [mcData.effectsByName.haste.id]: {
+            amplifier: 1,
+            duration: 60
+          }
+        }
+      )
+      expect(time).toBe(0)
+    })
+
+    test('instant break bedrock (creative)', () => {
+      const block = Block.fromStateId(mcData.blocksByName.bedrock.defaultState, 0)
+      const time = block.digTime(null, true, false, false, [], {})
+      expect(time).toBe(0)
+    })
   })
 })
