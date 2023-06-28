bb76b3347e51d9e466b7e1032f1c53b092d8deec
PondWader <66561610+PondWader@users.noreply.github.com>
2023-06-28 16:12:49 +0100

    Replace blockEntity test to test that checks NBT is set correctly

--- test/entity.test.js
+++ test/entity.test.js
@@ -31,23 +31,41 @@ describe('handles block entities', () => {
     })
 
     describe('signs', function () {
-      it('.blockEntity works on ' + version, () => {
+      it('Sets entity NBT correctly on ' + version, () => {
         const sign = Block.fromStateId(registry.blocksByName.standing_sign?.defaultState || registry.blocksByName.oak_sign?.defaultState)
-        const tag = {
-          pc: nbt.comp({
+        sign.setSignText(['Hello', 'World', '!', '?'])
+
+        if (registry.supportFeature('multiSidedSigns')) {
+          sign.setSignText(undefined, ['Back'])
+
+          assert.deepEqual(nbt.simplify(sign.entity), nbt.simplify(nbt.comp({
+            id: nbt.string('minecraft:sign'),
+            isWaxed: nbt.byte(0),
+            back_text: nbt.comp({
+              has_glowing_text: nbt.byte(0),
+              color: nbt.string('black'),
+              messages: nbt.list(nbt.string(['{"text":"Back"}']))
+            }),
+            front_text: nbt.comp({
+              has_glowing_text: nbt.byte(0),
+              color: nbt.string('black'),
+              messages: nbt.list(nbt.string(['{"text":"Hello"}', '{"text":"World"}', '{"text":"!"}', '{"text":"?"}']))
+            })
+          })))
+        } else if (registry.type === 'pc') {
+          assert.deepEqual(nbt.simplify(sign.entity), nbt.simplify(nbt.comp({
+            id: nbt.string(registry.version['>=']('1.11') ? 'minecraft:sign' : 'Sign'),
             Text1: nbt.string('{"text":"Hello"}'),
             Text2: nbt.string('{"text":"World"}'),
             Text3: nbt.string('{"text":"!"}'),
             Text4: nbt.string('{"text":"?"}')
-          }),
-          bedrock: nbt.comp({
-            Text: nbt.string('Hello\nWorld!')
-          })
-        }[registry.type]
-        const simplified = nbt.simplify(tag)
-        sign.entity = tag
-
-        assert.deepEqual(simplified.Text, sign.blockEntity.Text)
+          })))
+        } else if (registry.type === 'bedrock') {
+          assert.deepEqual(nbt.simplify(sign.entity), nbt.simplify(nbt.comp({
+            id: nbt.string('Sign'),
+            Text: nbt.string('Hello\nWorld\n!\n?')
+          })))
+        }
       })
 
       it('.signText works on ' + version, () => {
